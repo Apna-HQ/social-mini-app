d30c7efb778aab28e0314f95b26f8b584c28471e
Yadunandan Batchu <nandubatchu@gmail.com>
2024-12-18 20:15:08 +0000

    following/publishing/updating

--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,12 @@ export default function Home() {
   return (
     <main>
       <h1>Social</h1>
-      <button onClick={async () => {console.log(await apna.nostr.getProfile());}}>nostr.getProfile()</button>
-      <pre id="nostr.getProfile"></pre>
+      <button onClick={async () => {console.log(await apna.nostr.getProfile());}}>nostr.getProfile()</button><br></br>
+      <button onClick={async () => {const oldProfile = await apna.nostr.getProfile();oldProfile.metadata.about = `Bitcoin Enthusiast ${Date.now()}`;console.log(await apna.nostr.updateProfile(oldProfile));}}>nostr.updateProfile()</button><br></br>
+      <button onClick={async () => {console.log(await apna.nostr.followNpub("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e"));}}>nostr.followNpub()</button><br></br>
+      <button onClick={async () => {console.log(await apna.nostr.unfollowNpub("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e"));}}>nostr.unfollowNpub()</button><br></br>
+      <button onClick={async () => {console.log(await apna.nostr.publishNote(`Hello World ${Date.now()}`));}}>nostr.publishNote()</button><br></br>
+      
       {notes.map((note, index) => (
         <div key={index} style={{ border: "solid 1px" }}>
         {/* <Image width={50} height={50} alt="author" src={authorMetadata[note.pubkey]?.picture ?? "https://www.kindpng.com/picc/m/252-2524695_dummy-profile-image-jpg-hd-png-download.png"}/> */}
