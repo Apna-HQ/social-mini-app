781ccabab949c3641f7486d0b2b26f4d26cb63f3
Yadunandan Batchu <nandubatchu@gmail.com>
2025-01-01 20:36:48 +0000

    upgrade and update interface implementation

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         // @ts-ignore
         window.apna = apna;
       }   
-      console.log('nostr.getProfile return value: ', await apna.nostr.getProfile())
+      console.log('nostr.getProfile return value: ', await apna.nostr.getActiveUserProfile())
 
       // apna.nostr.subscribeToFeed("FOLLOWING_FEED",(e: any) => {
       //   setNotes((notes)=>[...notes, e])
@@ -42,7 +42,7 @@ export default function Home() {
     <main>
       <h1>Social</h1>
       <button onClick={async () => {
-          const profile = await apna.nostr.getProfile();
+          const profile = await apna.nostr.getActiveUserProfile();
           document.getElementById("profile")!.innerText = JSON.stringify(
             profile,
             undefined,
@@ -52,7 +52,7 @@ export default function Home() {
         }}>nostr.getProfile()</button><br></br>
       <pre id="profile"></pre>
       <button onClick={async () => {
-          const profile = await apna.nostr.getNpubProfile("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e");
+          const profile = await apna.nostr.fetchUserProfile("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e");
           document.getElementById("npub_profile")!.innerText = JSON.stringify(
             profile,
             undefined,
@@ -61,15 +61,15 @@ export default function Home() {
           console.log(profile);
         }}>nostr.getNpubProfile()</button><br></br>
       <pre id="npub_profile"></pre>
-      <button onClick={async () => {const oldProfile = await apna.nostr.getProfile();oldProfile.metadata.about = `Bitcoin Enthusiast ${Date.now()}`;console.log(await apna.nostr.updateProfile(oldProfile));}}>nostr.updateProfile()</button><br></br>
-      <button onClick={async () => {console.log(await apna.nostr.followNpub("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e"));}}>nostr.followNpub()</button><br></br>
-      <button onClick={async () => {console.log(await apna.nostr.unfollowNpub("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e"));}}>nostr.unfollowNpub()</button><br></br>
+      <button onClick={async () => {const oldProfile = await apna.nostr.getActiveUserProfile();oldProfile.metadata.about = `Bitcoin Enthusiast ${Date.now()}`;console.log(await apna.nostr.updateProfileMetadata(oldProfile.metadata));}}>nostr.updateProfile()</button><br></br>
+      <button onClick={async () => {console.log(await apna.nostr.followUser("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e"));}}>nostr.followNpub()</button><br></br>
+      <button onClick={async () => {console.log(await apna.nostr.unfollowUser("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e"));}}>nostr.unfollowNpub()</button><br></br>
       <button onClick={async () => {console.log(await apna.nostr.publishNote(`Hello World ${Date.now()}`));}}>nostr.publishNote()</button><br></br>
       <button onClick={async () => {console.log(await apna.nostr.likeNote(`note187j8dxwta5zvxle446uqutxue764q79vxmtv85dw7fnujlqgdm2qm7kelc`));}}>nostr.likeNote()</button><br></br>
       <button onClick={async () => {console.log(await apna.nostr.repostNote(`note187j8dxwta5zvxle446uqutxue764q79vxmtv85dw7fnujlqgdm2qm7kelc`, 'test quote'));}}>nostr.repostNote()</button><br></br>
       <button onClick={async () => {console.log(await apna.nostr.replyToNote(`note187j8dxwta5zvxle446uqutxue764q79vxmtv85dw7fnujlqgdm2qm7kelc`, `test reply ${Date.now()}`));}}>nostr.replyToNote()</button><br></br>
       <button onClick={async () => {setNotes([]);await apna.nostr.subscribeToFeed('FOLLOWING_FEED', (e: any) => {setNotes((notes)=>[...notes, e])});}}>nostr.subscribeToFeed()</button><br></br>
-      <button onClick={async () => {setNotes([]);await apna.nostr.subscribeToNpubFeed("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e", 'NOTES_FEED', (e: any) => {setNotes((notes)=>[...notes, e])});}}>nostr.subscribeToNpubFeed()</button><br></br>
+      <button onClick={async () => {setNotes([]);await apna.nostr.subscribeToUserFeed("npub1w46mjnagz9f0u556fzva8ypfftc5yfm32n8ygqmd2r32mxw4cfnsvkvy9e", 'NOTES_FEED', (e: any) => {setNotes((notes)=>[...notes, e])});}}>nostr.subscribeToNpubFeed()</button><br></br>
       
       {notes.map((note, index) => (
         <div key={index} style={{ border: "solid 1px" }}>
