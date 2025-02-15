9d326edfbb8dbcde6f5a99b315c7bf21f9065553
Yadunandan Batchu <nandubatchu@gmail.com>
2025-02-15 19:37:27 +0000

    keep default size and merge classnames

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import { cn } from "@/lib/utils"
 
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
@@ -7,7 +8,10 @@ const Avatar = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Root
     ref={ref}
-    className={`relative flex shrink-0 overflow-hidden rounded-full ${className}`}
+    className={cn(
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      className
+    )}
     {...props}
   />
 ))
@@ -19,7 +23,7 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className="aspect-square h-full w-full"
+    className={cn("aspect-square h-full w-full", className)}
     {...props}
   />
 ))
@@ -31,7 +35,10 @@ const AvatarFallback = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
     ref={ref}
-    className="flex h-full w-full items-center justify-center rounded-full bg-muted"
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      className
+    )}
     {...props}
   />
 ))
