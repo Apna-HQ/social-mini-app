0ba7e0366c17689c2e768a1ee2d7e7674d78a90a
Yadunandan Batchu <nandubatchu@gmail.com>
2025-02-17 20:00:14 +0000

    expose storybook

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    async rewrites() {
+        return [
+            {
+                source: '/storybook',
+                destination: '/storybook/index.html',
+            },
+            {
+                source: '/storybook/:path*',
+                destination: '/storybook/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
