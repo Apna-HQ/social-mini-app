8445b7ba3d0f7a4845a8e17bd756a2b3df21f17a
Yadunandan Batchu <nandubatchu@gmail.com>
2025-02-17 20:08:37 +0000

    Revert "expose storybook"
    This reverts commit 0ba7e0366c17689c2e768a1ee2d7e7674d78a90a.

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    async rewrites() {
-        return [
-            {
-                source: '/storybook',
-                destination: '/storybook/index.html',
-            },
-            {
-                source: '/storybook/:path*',
-                destination: '/storybook/:path*',
-            },
-        ];
-    },
 };
 
 export default nextConfig;
